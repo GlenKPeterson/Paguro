5ab580c234cd73b3a496677888acbf11462285e1
Glen K. Peterson <glen@organicdesign.org>
2015-04-12 14:56:09 -0400

    Updated SequenceTest to include all the tests of ViewTest.

--- src/test/java/org/organicdesign/fp/permanent/SequenceTest.java
+++ src/test/java/org/organicdesign/fp/permanent/SequenceTest.java
@@ -2,6 +2,7 @@ package org.organicdesign.fp.permanent;
 
 import org.junit.Test;
 import org.organicdesign.fp.Option;
+import org.organicdesign.fp.ephemeral.View;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -38,4 +39,44 @@ public class SequenceTest {
         assertEquals(Option.of(3), seq.filter(i -> i > 2).first());
         assertEquals(Option.none(), seq.filter(i -> i > 10).first());
     }
+
+    @Test
+    public void takeAndDrop() {
+        assertArrayEquals(Sequence.ofArray(1, 2, 3, 4, 5, 6, 7, 8, 9)
+                                  .drop(0).take(8888).toArray(),
+                          new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        assertArrayEquals(Sequence.ofArray(1,2,3,4,5,6,7,8,9)
+                                  .drop(1).take(1).toArray(),
+                   new Integer[] { 2 });
+        assertArrayEquals(Sequence.ofArray(1,2,3,4,5,6,7,8,9)
+                                  .drop(2).take(2).toArray(),
+                   new Integer[] { 3,4 });
+        assertArrayEquals(Sequence.ofArray(1,2,3,4,5,6,7,8,9)
+                                  .drop(3).take(3).toArray(),
+                   new Integer[] { 4,5,6 });
+        assertArrayEquals(Sequence.ofArray(1, 2, 3, 4, 5, 6, 7, 8, 9)
+                                  .drop(9999).take(3).toArray(),
+                          new Integer[]{});
+        assertArrayEquals(Sequence.ofArray(1,2,3,4,5,6,7,8,9)
+                                  .drop(3).take(0).toArray(),
+                   new Integer[] { });
+    }
+
+    @Test
+    public void chain1() {
+        assertArrayEquals(Sequence.ofArray(5)                      //         5
+                                  .prepend(Sequence.ofArray(4))    //       4,5
+                                  .append(Sequence.ofArray(6))     //       4,5,6
+                                  .prepend(Sequence.ofArray(2, 3)) //   2,3,4,5,6
+                                  .append(Sequence.ofArray(7, 8))  //   2,3,4,5,6,7,8
+                                  .prepend(Sequence.ofArray(1))    // 1,2,3,4,5,6,7,8
+                                  .append(Sequence.ofArray(9))     // 1,2,3,4,5,6,7,8,9
+                                  .filter(i -> i > 3)              //       4,5,6,7,8,9
+                                  .map(i -> i - 2)                 //   2,3,4,5,6,7
+                                  .take(5)                         //   2,3,4,5,6
+                                  .drop(2)                         //       4,5,6
+                                  .toArray(),
+                          new Integer[]{4, 5, 6});
+    }
+
 }
