e9227fcfb362923b41b766cd6f0eb7762caebf1b
Glen K. Peterson <glen@organicdesign.org>
2015-05-20 11:10:18 -0400

    Added Either.

--- src/main/java/org/organicdesign/fp/Either.java
+++ src/main/java/org/organicdesign/fp/Either.java
@@ -1,3 +1,16 @@
+// Copyright 2015 PlanBase Inc. & Glen Peterson
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package org.organicdesign.fp;
 
 import java.util.Objects;
@@ -8,6 +21,8 @@ import org.organicdesign.fp.function.Function1;
  Where Option repesents the presence or absence of a value, Either represents the presence of one value OR another.
  By convention, Left is often used like Option.None, but stores error codes or objects.
  Right is used like Option.Some.
+ Loosely based on and/or inspired by Scala's
+ <a href="http://www.scala-lang.org/api/rc2/scala/Either.html" target="_blank">Either</a>.
  */
 public interface Either<L,R> {
     /** Returns true if this Either has a left value. */
