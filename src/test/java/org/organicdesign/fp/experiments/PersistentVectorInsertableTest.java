801eb274daf6d7160af4ac920c781dec1278938c
Glen K. Peterson <glen@organicdesign.org>
2015-05-19 22:44:40 -0400

    Updated comments.

--- src/test/java/org/organicdesign/fp/experiments/PersistentVectorInsertableTest.java
+++ src/test/java/org/organicdesign/fp/experiments/PersistentVectorInsertableTest.java
@@ -8,7 +8,7 @@ public class PersistentVectorInsertableTest {
     @Test public void basicAppends() {
         PersistentVectorInsertable<Integer> pvi = PersistentVectorInsertable.empty();
         System.out.println(pvi.toString());
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             pvi = pvi.insert(i, i);
             System.out.println("=====================================================\n" +
                                pvi.toString() +
@@ -19,9 +19,15 @@ public class PersistentVectorInsertableTest {
         System.out.println("\n\nFinal Vector:");
         System.out.println(pvi.toString());
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             assertEquals(Integer.valueOf(i), pvi.get(i));
        }
+// Hey, it's even balanced - Yay!
+// Branch(10, Array<Branch>(Branch(4, Array<Leaf>(Leaf(2,Array<Integer>(0,1)),
+//                                                Leaf(2,Array<Integer>(2,3)))),
+//                          Branch(6, Array<Leaf>(Leaf(2,Array<Integer>(4,5)),
+//                                                Leaf(4,Array<Integer>(6,7,8,9))))))
+
     }
 
 }
