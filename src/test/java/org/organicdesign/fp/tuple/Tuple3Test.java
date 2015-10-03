b35a5b067a17e98fef4e15573b13a574d833b248
Glen K. Peterson <glen@organicdesign.org>
2015-10-03 09:30:49 -0400

    Reworked unit test with an aim of maybe automating the creation of these tests.

--- src/test/java/org/organicdesign/fp/tuple/Tuple3Test.java
+++ src/test/java/org/organicdesign/fp/tuple/Tuple3Test.java
@@ -23,35 +23,41 @@ import static org.organicdesign.fp.testUtils.EqualsContract.equalsSameHashCode;
 public class Tuple3Test {
     @Test
     public void constructionAndAccess() {
-        Tuple3<Integer,String,Boolean> a = Tuple3.of(3, "Hello", true);
+        Tuple3<Integer,String,Boolean> a = Tuple3.of(3, "2nd", true);
 
         assertEquals(new Integer(3), a._1());
-        assertEquals("Hello", a._2());
+        assertEquals("2nd", a._2());
         assertTrue(a._3());
 
-        Tuple3<Integer,String,Boolean> b = Tuple3.of(5, "hello", false);
+        Tuple3<String,Integer,Boolean> b = Tuple3.of("1st", 5, false);
 
-        assertEquals(new Integer(5), b._1());
-        assertEquals("hello", b._2());
+        assertEquals("1st", b._1());
+        assertEquals(Integer.valueOf(5), b._2());
         assertFalse(b._3());
 
-        Tuple3<Integer,String,Boolean> c = Tuple3.of(7, null, null);
+        Tuple3<Boolean,Integer,String> c = Tuple3.of(true, null, null);
 
-        assertEquals(new Integer(7), c._1());
+        assertTrue(c._1());
         assertEquals(null, c._2());
         assertEquals(null, c._3());
 
-        Tuple3<Integer,String,Boolean> d = Tuple3.of(null, "Hello", null);
+        Tuple3<Integer,String,Boolean> d = Tuple3.of(null, "2nd", null);
 
         assertEquals(null, d._1());
-        assertEquals("Hello", d._2());
+        assertEquals("2nd", d._2());
         assertEquals(null, d._3());
 
-        equalsDistinctHashCode(a, Tuple3.of(3, "Hello", true), Tuple3.of(3, "Hello", true),
-                               Tuple3.of(3, "Hello", false));
+        Tuple3<?,?,Integer> e = Tuple3.of(null, null, 9);
 
-        equalsSameHashCode(a, Tuple3.of(3, "Hello", true), Tuple3.of(3, "Hello", true),
-                           Tuple3.of("Hello", 3, true));
+        assertEquals(null, e._1());
+        assertEquals(null, e._2());
+        assertEquals(Integer.valueOf(9), e._3());
+
+        equalsDistinctHashCode(a, Tuple3.of(3, "2nd", true), Tuple3.of(3, "2nd", true),
+                               Tuple3.of(3, "2nd", false));
+
+        equalsSameHashCode(a, Tuple3.of(3, "2nd", true), Tuple3.of(3, "2nd", true),
+                           Tuple3.of("2nd", 3, true));
 
         assertEquals("Tuple3(hi,11,true)", Tuple3.of("hi", 11, true).toString());
     }
