ec7ca0ef9b3ed19c756c44e90d755f44f33283f0
Glen K. Peterson <glen@organicdesign.org>
2017-05-13 22:15:04 -0400

    Filled out tests.

--- paguro-bench/src/main/java/org/organicdesign/fp/MyBenchmark.java
+++ paguro-bench/src/main/java/org/organicdesign/fp/MyBenchmark.java
@@ -27,18 +27,70 @@ public class MyBenchmark {
         return last;
     }
 
-    @Benchmark public void BuildRrb100000() { buildList(RrbTree1.empty(), 100000); }
-    @Benchmark public void IterateRrb100000(Rrb100000 rrb) { iterateList(rrb.rrb); }
-
+    @State(Scope.Thread) public static class Rrb10 {
+        public ImList<Integer> rrb = buildList(RrbTree1.empty(), 10);
+    }
+    @State(Scope.Thread) public static class Rrb100 {
+        public ImList<Integer> rrb = buildList(RrbTree1.empty(), 100);
+    }
+    @State(Scope.Thread) public static class Rrb1000 {
+        public ImList<Integer> rrb = buildList(RrbTree1.empty(), 1000);
+    }
+    @State(Scope.Thread) public static class Rrb10000 {
+        public ImList<Integer> rrb = buildList(RrbTree1.empty(), 10000);
+    }
     @State(Scope.Thread) public static class Rrb100000 {
         public ImList<Integer> rrb = buildList(RrbTree1.empty(), 100000);
     }
+    @State(Scope.Thread) public static class Rrb1000000 {
+        public ImList<Integer> rrb = buildList(RrbTree1.empty(), 1000000);
+    }
 
+    @State(Scope.Thread) public static class Vec10 {
+        public ImList<Integer> vec = buildList(PersistentVector.empty(), 10);
+    }
+    @State(Scope.Thread) public static class Vec100 {
+        public ImList<Integer> vec = buildList(PersistentVector.empty(), 100);
+    }
+    @State(Scope.Thread) public static class Vec1000 {
+        public ImList<Integer> vec = buildList(PersistentVector.empty(), 1000);
+    }
+    @State(Scope.Thread) public static class Vec10000 {
+        public ImList<Integer> vec = buildList(PersistentVector.empty(), 10000);
+    }
     @State(Scope.Thread) public static class Vec100000 {
         public ImList<Integer> vec = buildList(PersistentVector.empty(), 100000);
     }
+    @State(Scope.Thread) public static class Vec1000000 {
+        public ImList<Integer> vec = buildList(PersistentVector.empty(), 1000000);
+    }
 
+    @Benchmark public void BuildRrb10() { buildList(RrbTree1.empty(), 10); }
+    @Benchmark public void BuildRrb100() { buildList(RrbTree1.empty(), 100); }
+    @Benchmark public void BuildRrb1000() { buildList(RrbTree1.empty(), 1000); }
+    @Benchmark public void BuildRrb10000() { buildList(RrbTree1.empty(), 10000); }
+    @Benchmark public void BuildRrb100000() { buildList(RrbTree1.empty(), 100000); }
+    @Benchmark public void BuildRrb1000000() { buildList(RrbTree1.empty(), 1000000); }
+
+    @Benchmark public void BuildVec10() { buildList(PersistentVector.empty(), 10); }
+    @Benchmark public void BuildVec100() { buildList(PersistentVector.empty(), 100); }
+    @Benchmark public void BuildVec1000() { buildList(PersistentVector.empty(), 1000); }
+    @Benchmark public void BuildVec10000() { buildList(PersistentVector.empty(), 10000); }
     @Benchmark public void BuildVec100000() { buildList(PersistentVector.empty(), 100000); }
+    @Benchmark public void BuildVec1000000() { buildList(PersistentVector.empty(), 1000000); }
+
+    @Benchmark public void IterateRrb10(Rrb10 rrb) { iterateList(rrb.rrb); }
+    @Benchmark public void IterateRrb100(Rrb100 rrb) { iterateList(rrb.rrb); }
+    @Benchmark public void IterateRrb1000(Rrb1000 rrb) { iterateList(rrb.rrb); }
+    @Benchmark public void IterateRrb10000(Rrb10000 rrb) { iterateList(rrb.rrb); }
+    @Benchmark public void IterateRrb100000(Rrb100000 rrb) { iterateList(rrb.rrb); }
+    @Benchmark public void IterateRrb1000000(Rrb1000000 rrb) { iterateList(rrb.rrb); }
+
+    @Benchmark public void IterateVec10(Vec10 vec) { iterateList(vec.vec); }
+    @Benchmark public void IterateVec100(Vec100 vec) { iterateList(vec.vec); }
+    @Benchmark public void IterateVec1000(Vec1000 vec) { iterateList(vec.vec); }
+    @Benchmark public void IterateVec10000(Vec10000 vec) { iterateList(vec.vec); }
     @Benchmark public void IterateVec100000(Vec100000 vec) { iterateList(vec.vec); }
+    @Benchmark public void IterateVec1000000(Vec1000000 vec) { iterateList(vec.vec); }
 
 }
