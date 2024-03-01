7f4a655812e03230f901389531cff10859f13e2c
Sam Gammon <sam@elide.ventures>
2024-02-29 21:34:38 -0800

    feat: add jpms support
    - feat: add `module-info.java` descriptor
    - chore: update all maven plugins to latest
    - chore: bump version → `3.11.0`
    
    Users can now use this module via the Java Platform Module System,
    with the following `requires` declaration in their module:
    
    ```java
    module my.module {
      requires paguro;
    }
    ```
    
    Signed-off-by: Sam Gammon <sam@elide.ventures>

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module paguro {
+    requires java.base;
+    requires static org.jetbrains.annotations;
+
+    exports org.organicdesign.fp;
+    exports org.organicdesign.fp.collections;
+    exports org.organicdesign.fp.function;
+    exports org.organicdesign.fp.indent;
+    exports org.organicdesign.fp.oneOf;
+    exports org.organicdesign.fp.tuple;
+    exports org.organicdesign.fp.type;
+    exports org.organicdesign.fp.xform;
+}
